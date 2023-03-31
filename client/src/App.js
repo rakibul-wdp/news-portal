097aa78c5b7ebdc988c43cf46e981164115aaec9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-31 15:17:43 +0600

    create simple ui of game overview

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {game.isStarted ? (
+        <>
+          <h2>{`Player: ${game.username} Score: ${game.score}`}</h2>
+          <div>
+            {game.deck.map((card, index) => (
+              <div key={index}>{card}</div>
+            ))}
+          </div>
+          <button onClick={handleDrawCard}>Draw Card</button>
+          <button onClick={handleEndGame}>End Game</button>
+        </>
+      ) : (
+        <>
+          <h2>Enter your username to start the game:</h2>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <button onClick={handleStartGame}>Start Game</button>
+        </>
+      )}
     </div>
   );
 }
