f6f95958e09d4e21f3cf113517803fea03460323
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-31 14:15:12 +0600

    run server with basic setup

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,11 @@
-export {};
+import express from "express";
+const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+const port = 5000;
+server.listen(port, () => {
+    console.log(`Server io running on port ${port}`);
+});
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-import fs from "fs";
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
