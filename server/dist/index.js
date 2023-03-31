9c0c674728d49b950eb6761f8cb944924e531410
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-31 14:28:35 +0600

    wow database is connected

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 import http from 'http';
 const server = http.createServer(app);
@@ -6,6 +9,17 @@ import { Server } from "socket.io";
 const io = new Server(server);
 const port = 5000;
 server.listen(port, () => {
-    console.log(`Server io running on port ${port}`);
+    console.log(`Server running on port ${port}`);
+});
+mongoose
+    .connect(process.env.DB_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+    console.log("MongoDB connected");
+})
+    .catch((error) => {
+    console.log(error);
 });
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 import http from 'http';
 const server = http.createServer(app);
@@ -11,3 +13,15 @@ const port = 5000;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+mongoose
+  .connect(process.env.DB_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as mongoose.ConnectOptions)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
