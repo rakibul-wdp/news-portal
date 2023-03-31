7cbb550fb2621834062755548454b7702d916187
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-01 00:16:00 +0600

    create user data model

---
+++ server/src/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
