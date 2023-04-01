3a4c7964ce6d6fb8e151ec3d7961d322a195184d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-01 17:52:49 +0600

    again update liver server

--- client/src/components/GameCards.js
+++ client/src/components/GameCards.js
@@ -80,7 +80,7 @@ const GameCards = () => {
   const exitGame = async () => {
     const maxScore = Math.max(score + user.score, score);
 
-    await axios.post("https://card-game-server.onrender.com/updatescore", {
+    await axios.post("https://card-game-server.onrender.com/api/updatescore", {
       username: user.username,
       score: score,
     }).then(res => {
